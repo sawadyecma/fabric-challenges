3f97973593f399f10412a371b5ced7546a4da36c
sawadyecma <snowslidez@yahoo.co.jp>
2022-06-19 22:22:21 +0900

    002 genogram

--- web/cases/002/index.ts
+++ web/cases/002/index.ts
@@ -1,5 +1,32 @@
+import { fabric } from "fabric";
 import { startHook } from "../common/startHook";
 
-const app = startHook();
+const { app, canvasEl } = startHook();
+
+const canvas = new fabric.Canvas(canvasEl);
+
+const male = new fabric.Rect({
+  width: 100,
+  height: 100,
+  fill: undefined,
+  strokeWidth: 5,
+  stroke: "black",
+});
+
+const female = new fabric.Circle({
+  width: 100,
+  height: 100,
+  fill: undefined,
+  radius: 50,
+  strokeWidth: 5,
+  stroke: "black",
+});
+
+const persons: fabric.Object[] = [male, female];
+
+persons.forEach((person, i) => {
+  person.set({ left: i * 100 });
+  canvas.add(person);
+});
 
 export {};
