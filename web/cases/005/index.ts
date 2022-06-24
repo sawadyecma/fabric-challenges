e1888c4fd1805a9a253dde9b06d6b9cbd5586edf
sawadyecma <snowslidez@yahoo.co.jp>
2022-06-24 23:33:33 +0900

    scale keep for checkbox

--- web/cases/005/index.ts
+++ web/cases/005/index.ts
@@ -2,6 +2,7 @@ import { startHook } from "../common/startHook";
 import { fabric } from "fabric";
 import { prepareToSvgArea } from "../common/prepareToSvgArea";
 import { composeSvg } from "../common/fabricModules/svg";
+import { Checkbox } from "../common/fabricModules/checkbox";
 
 const { app, canvasEl } = startHook();
 
@@ -34,7 +35,11 @@ checkboxOff.set({ left: 200, top: 0 });
 
 canvas.add(checkboxOff);
 
-const checkboxOn = await composeSvg("/icons/checkbox/on.svg");
+const checkboxOff1 = new Checkbox(false);
+checkboxOff1.set({ left: 200, top: 200 });
+canvas.add(checkboxOff1);
+
+const checkboxOn = new Checkbox(true);
 checkboxOn.set({ left: 200, top: 40 });
 canvas.add(checkboxOn);
 
@@ -48,6 +53,19 @@ app.appendChild(toSvgButton);
 function onToSvgButtonClick() {
   const svg = canvas.toSVG() ?? "";
   svgArea.innerHTML = svg;
+}
+
+const toggleCheckButton = document.createElement("button");
+toggleCheckButton.innerText = "toggleCheckbox";
+toggleCheckButton.addEventListener("click", onToggleCheckButtonClick);
+app.appendChild(toggleCheckButton);
+
+async function onToggleCheckButtonClick() {
+  const cur = checkboxOn.getChecked();
+  console.log(cur);
+  await checkboxOn.setChecked(!cur);
+  // checkboxOn.render();
+  canvas.renderAll();
 }
 
 export {};
---
+++ web/cases/common/fabricModules/checkbox.ts
@@ -0,0 +1,63 @@
+import { fabric } from "fabric";
+
+function getSvg(checked: boolean) {
+  const svgUrl = `/icons/checkbox/${checked ? "on" : "off"}.svg`;
+  return new Promise<fabric.Group>((resolve) => {
+    fabric.loadSVGFromURL(svgUrl, (objects) => {
+      console.log(objects);
+      resolve(new fabric.Group(objects));
+    });
+  });
+}
+
+const checkboxOff = await getSvg(false);
+const checkboxOn = await getSvg(true);
+
+export class Checkbox extends fabric.Group {
+  private checked: boolean = false;
+
+  constructor(checked: boolean) {
+    super();
+    if (checked) {
+      checkboxOn.forEachObject((o) => {
+        this.addWithUpdate(o);
+      });
+    }
+    this.checked = checked;
+  }
+
+  getChecked() {
+    return this.checked;
+  }
+
+  async setChecked(checked: boolean) {
+    if (this.checked === checked) return;
+
+    const left = this.left!;
+    const top = this.top!;
+    const width = this.width!;
+    const height = this.height!;
+    const scaleX = this.scaleX!;
+    const scaleY = this.scaleY!;
+
+    this.remove(...this._objects);
+
+    const target = checked ? checkboxOn : checkboxOff;
+
+    target.forEachObject((o) => {
+      this.addWithUpdate(o);
+    });
+
+    this.set("left", left);
+    this.set("top", top);
+    this.set("width", width);
+    this.set("height", height);
+    this.set("scaleX", scaleX);
+    this.set("scaleY", scaleY);
+    this.set("hasControls", true);
+    this.set("selectable", true);
+
+    this.checked = checked;
+    return;
+  }
+}
