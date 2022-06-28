a0ec8d89b0cdb9bf174818723bb946e7d9e647a8
sawadyecma <snowslidez@yahoo.co.jp>
2022-06-28 18:01:36 +0900

    fix Slider

--- react/src/Slider.tsx
+++ react/src/Slider.tsx
@@ -1,12 +1,45 @@
-import { RangeInput } from "grommet";
-import { useState } from "react";
+import * as Icons from "grommet-icons";
+import { Box, Button, RangeInput } from "grommet";
+import { ChangeEvent, useState } from "react";
+
+const min = 5;
+const max = 25;
 
 export const Slider = () => {
   const [value, setValue] = useState(10);
+
+  const onChange = (event: ChangeEvent<HTMLInputElement>) => {
+    const newValue = parseInt(event.target.value);
+    setValue(newValue);
+  };
+
   return (
-    <RangeInput
-      value={value}
-      onChange={(event) => setValue(parseInt(event.target.value))}
-    />
+    <Box direction="row" align="center" gap="small">
+      <Button
+        plain={false}
+        disabled={value <= min}
+        icon={<Icons.Subtract color="neutral-2" />}
+        onClick={() => {
+          setValue((value) => value - 1);
+        }}
+      />
+      <Box align="center" width="small">
+        <RangeInput
+          min={min}
+          max={max}
+          step={1}
+          value={value}
+          onChange={onChange}
+        />
+      </Box>
+      <Button
+        plain={false}
+        disabled={value >= max}
+        icon={<Icons.Add color="neutral-2" />}
+        onClick={() => {
+          setValue((value) => value + 1);
+        }}
+      />
+    </Box>
   );
 };
