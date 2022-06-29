d33c270214e954dd9fe25d7ccd41f1d6f1978566
sawadyecma <snowslidez@yahoo.co.jp>
2022-06-29 11:15:00 +0900

    install react-rouer-dom

--- react/src/main.tsx
+++ react/src/main.tsx
@@ -3,11 +3,16 @@ import ReactDOM from "react-dom/client";
 import { App } from "./App";
 import { Grommet } from "grommet";
 import { theme } from "./theme";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <Grommet theme={theme}>
-      <App />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<App />} />
+        </Routes>
+      </BrowserRouter>
     </Grommet>
   </React.StrictMode>
 );
